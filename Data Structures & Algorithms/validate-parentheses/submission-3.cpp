77ae6bc83b1e7cec1f170df2195e60172c5acc51
draco <dracofs.777@gmail.com>
2026-05-19 18:58:52 -0700

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string str) {
+        stack<char> s;
+        unordered_map<char, char> mmap = {
+            {'}', '{'},
+            {']', '['},
+            {')', '('}
+        };
+        for (char c : str) {
+            if (!mmap.contains(c)) s.push(c);
+            else {
+                if (s.empty() || mmap[c] != s.top()) return false;
+                s.pop();
+            }
+        }
+        
+        return s.empty();
+    }
+};
