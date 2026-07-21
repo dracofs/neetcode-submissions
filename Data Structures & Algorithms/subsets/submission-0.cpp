c0dddf1bdd6a4ef3a843e1718c9cac18551083ff
draco <dracofs.777@gmail.com>
2026-07-21 16:37:17 -0300

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        gen(nums, res, {}, 0);
+        return res;
+    }
+
+    void gen(vector<int>& nums, vector<vector<int>>& res, vector<int> curr, int idx) {
+        if (idx >= nums.size()) {
+            res.push_back(curr);
+            return;
+        }
+
+        // either include num or dont
+        curr.push_back(nums[idx]);
+        gen(nums, res, curr, idx + 1);
+        curr.pop_back();
+        gen(nums, res, curr, idx + 1);
+        return;
+    }
+};
