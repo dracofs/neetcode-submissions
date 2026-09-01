34fc35c91e49d93b5c180bfbb74114df45022d80
draco <dracofs.777@gmail.com>
2026-08-31 19:20:30 -0700

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            // even
+            int l = i, r = i + 1;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                count++;
+                l--, r++;
+            }
+
+            // odd
+            l = i, r = i;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                count++;
+                l--, r++;
+            }
+        }
+
+        return count;
+    }
+};
