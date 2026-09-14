aabcd0be5c7966c381fd59796b555ac935e383d7
draco <dracofs.777@gmail.com>
2026-09-14 16:38:41 -0700

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<int> indeg(n);
+        vector<vector<int>> outdeg(n);
+
+        for (auto& prereq : prerequisites) {
+            indeg[prereq[0]]++;
+            outdeg[prereq[1]].push_back(prereq[0]);
+        }
+
+        queue<int> q;
+        vector<int> res;
+        for (int i{0}; i < n; i++) {
+            if (indeg[i] == 0) {
+                res.push_back(i);
+                q.push(i);
+            }
+        }
+
+        while (!q.empty()) {
+            int top = q.front(); q.pop();
+            for (int neighb : outdeg[top]) {
+                indeg[neighb]--;
+                if (indeg[neighb] == 0) {
+                    q.push(neighb);
+                    res.push_back(neighb);
+                }
+            }
+        }
+        if (res.size() != n) return {};
+        else return res;
+    }
+};
